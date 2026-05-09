7151be3cf594ecb095445994e16fe53525c511db
themedworld <99541159+themedworld@users.noreply.github.com>
2026-05-09 23:47:54 +0100

    first commit3

--- main.py
+++ main.py
@@ -2,27 +2,28 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import joblib
 import pandas as pd
-import numpy as np
+import os
 from fastapi.middleware.cors import CORSMiddleware
 
-# =========================
-# Charger le modèle
-# =========================
-MODEL_PATH = "./best_estimatedHours_task_callcenter_lgbm.joblib"
+MODEL_PATH = "best_estimatedHours_task_callcenter_lgbm.joblib"
+
+print("FILES:", os.listdir("."))
+
+if not os.path.exists(MODEL_PATH):
+    raise Exception(f"Model file not found: {MODEL_PATH}")
+
 model = joblib.load(MODEL_PATH)
 
 app = FastAPI(title="Estimated Hours API", version="1.0")
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # ⚠️ en prod tu peux mettre ton domaine frontend
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# =========================
-# Schéma input (Pydantic)
-# =========================
 class TaskInput(BaseModel):
     type: str
     priority: str
@@ -37,25 +38,12 @@ class TaskInput(BaseModel):
     reopenCount: int
     delayHours: float
 
-
-# =========================
-# Health check
-# =========================
 @app.get("/")
 def home():
     return {"message": "API is running 🚀"}
 
-# =========================
-# Prediction endpoint
-# =========================
 @app.post("/predict")
 def predict(data: TaskInput):
-    # Convertir input en DataFrame
     df = pd.DataFrame([data.dict()])
-
-    # Prédiction (pipeline = preprocess + model)
-    prediction = model.predict(df)
-
-    return {
-        "estimatedHours": float(prediction[0])
-    }
+    pred = model.predict(df)
+    return {"estimatedHours": float(pred[0])}
